3a61bbe498c054a0cb8e1210ef8badcdcfe7f74e
Jacob Vielweber <jnvielweber@gmail.com>
2022-11-22 00:00:13 -0600

    Create useFetch custom hook

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,12 @@
+import apiService from "./services/api.service";
+import { useState } from "react";
+
+export default function useFect() {
+  const [users, setUsers] = useState([]);
+
+  // get all users
+  // apiService.getAllUsers().then((users) => setUsers(users));
+
+  // return the users
+  return [users, setUsers];
+}
