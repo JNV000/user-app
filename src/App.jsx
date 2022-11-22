11ac7c17a0519ff28b65468f864afe9c6f26f1b2
Jacob Vielweber <jnvielweber@gmail.com>
2022-11-22 00:08:16 -0600

    Use useFetch hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import Login from "./components/Login";
+// import Login from "./components/Login";
 import React, { useEffect } from "react"; // could also import useEffect directly
 import apiService from "./services/api.service";
+import useFetch from "./hooks/useFetch";
 
 function App() {
+  const [users, setUsers] = useFetch();
+
+  useEffect(() => {
+    apiService.getAllUsers().then((users) => setUsers(users));
+  }, []);
+
   return (
-    <main className="flex justify-center">
-      <Login />
-    </main>
+    <>
+      <h1 className="text-3xl font-bold underline">Hello World</h1>
+      <ul>
+        {users.length ? (
+          users.map((user) => <li key={user.id}>{user.name}</li>)
+        ) : (
+          <li>Loading...</li>
+        )}
+      </ul>
+    </>
   );
 }
 
